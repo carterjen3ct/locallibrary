8b3a5527f10adcda9e122ac679be5946d31a0790
Carter Jensen <carter.t.jensen@gmail.com>
2026-09-09 15:44:33 -0400

    prep 02 added challenge code

--- catalog/admin.py
+++ catalog/admin.py
@@ -14,11 +14,16 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
@@ -31,9 +36,11 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BooksInstanceInline]
+
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
