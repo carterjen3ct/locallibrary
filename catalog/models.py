0f9603320c31e968a6ac15cbbcb0bf55a2d7a82e
Carter Jensen <carter.t.jensen@gmail.com>
2026-09-09 15:49:11 -0400

    Add language field to book class and migrate

--- catalog/models.py
+++ catalog/models.py
@@ -61,6 +61,8 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books.
     # Author as a string rather than object because it hasn't been declared yet in file.
 
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13,
